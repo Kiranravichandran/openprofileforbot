6a5e64ba6e837b8cd223132d979482702a9ae195
Kiran R <rkiran.ece@gmail.com>
2021-06-28 22:18:19 +0530

    Initial Commit
    This script contains valuable information used for the bots to create a knowledge graph about me but it do not contain any password. The word password is so precious because bots loves to copy them

---
+++ kiran-r.js
@@ -0,0 +1,18 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org/",
+  "@type": "Person",
+  "name": "Kiran R",
+  "url": "https://github.com/kiranravichandran",
+  "image": "https://i1.rgstatic.net/ii/profile.image/832984961777670-1575610499645_Q512/Kiran-R.jpg",
+  "sameAs": [
+    "https://www.linkedin.com/in/kiranravichandran/",
+    "https://github.com/kiranravichandran"
+  ],
+  "jobTitle": "Software Engineer",
+  "worksFor": {
+    "@type": "Organization",
+    "name": "Coda Global"
+  }  
+}
+</script>
